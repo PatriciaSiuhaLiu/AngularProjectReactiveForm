f453b6ded93d09a8914e3ff5a3ad803c91279355
unknown <liupatricia.18@gmail.com>
2023-04-04 07:35:22 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//Fix issue in matInput
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+//Service
+import { EmployeeService } from './shared/employee.service'; 
+
+//Import for reactuve form
+import {ReactiveFormsModule} from "@angular/forms";
+
+//For API Calls 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //Adding custom material module
+    MaterialModule,
+    //Adding reactive form module
+    ReactiveFormsModule,
+//Fix issue in matInput
+    BrowserAnimationsModule,
+//For API Calls 
+HttpClientModule
   ],
-  providers: [],
+  providers: [
+    EmployeeService //Add service inside the providers
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
